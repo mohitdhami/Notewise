eaf8f0fbfd448cc26c93e7ac6edc78dbbb41d7a5
mohitdhami <mohitdhamipth24@gmail.com>
2023-10-17 13:05:54 +0530

    feature introduction: github sign-up and login

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 import {Logo} from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth} from "convex/react";
+import {SignInButton} from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
 
 export const Navbar = () => {
     const {isAuthenticated, isLoading} = useConvexAuth();
@@ -15,6 +17,27 @@ export const Navbar = () => {
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <p>Loading...</p>
+                )}
+                {!isAuthenticated && !isLoading && (
+                        <>
+                            <SignInButton mode="modal">
+                                <Button  variant="ghost" size="sm">
+                                    Log in
+                                </Button>
+
+                            </SignInButton>
+
+                            <SignInButton mode="modal">
+                                <Button size="sm">
+                                    Get Jotion free
+                                </Button>
+
+                            </SignInButton>
+                        </>
+                    )
+                }
                 <ModeToggle />
             </div>
         </div>
