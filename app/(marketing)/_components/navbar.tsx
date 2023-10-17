6d05e0a266f6172aa93b325008b1fa80edaf665d
mohitdhami <mohitdhamipth24@gmail.com>
2023-10-17 13:55:56 +0530

    github login service is now active for use

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,10 @@ import { cn } from "@/lib/utils";
 import {Logo} from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useConvexAuth} from "convex/react";
-import {SignInButton} from "@clerk/clerk-react";
+import {SignInButton, UserButton} from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 export const Navbar = () => {
     const {isAuthenticated, isLoading} = useConvexAuth();
@@ -18,7 +20,7 @@ export const Navbar = () => {
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
                 {isLoading && (
-                    <p>Loading...</p>
+                    <Spinner />
                 )}
                 {!isAuthenticated && !isLoading && (
                         <>
@@ -36,8 +38,19 @@ export const Navbar = () => {
 
                             </SignInButton>
                         </>
-                    )
-                }
+                    )}
+
+                    {isAuthenticated && !isLoading && (
+                        <>
+                        <Button variant="ghost" size="sm" asChild>
+                            <Link href="/documents">
+                                Enter Jotion
+                            </Link>
+                        </Button>
+                        <UserButton afterSignOutUrl = "/"/>
+                        </>
+                    )}
+
                 <ModeToggle />
             </div>
         </div>
